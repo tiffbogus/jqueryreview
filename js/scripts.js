d148565e63755a3be868731fb76d100098ca5715
Tiffany <tiffany.bogus@gmail.com>
2015-10-09 21:48:44 -0700

    Add list to index.html; add info for form parts to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var icecreamInput = $("input#icecream").val();
+    var musicInput = $("input#music").val();
+    var movieInput= $("input#movie").val();
+    var colorInput = $("input#color").val();
+    var seasonInput = $("input#season").val();
+    var dinosaurInput = $("input#dinosaur").val();
 
+    $(".icecream").text(icecreamInput);
+    $(".music").text(musicInput);
+    $(".movie").text(movieInput);
+    $(".color").text(colorInput);
+    $(".season").text(seasonInput);
+    $(".dinosaur").text(dinosaurInput);
+
+    $("#favelist").show();
+
+    event.preventDefault();
+  });
 });
